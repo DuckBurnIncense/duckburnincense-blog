6510c55ff13ca73cd3fd8d9af91058e23d85b6b4
DuckBurnIncense <duckburnincense@outlook.com>
2025-10-24 11:25:33 +0800

    增加友链: 墨韵先知社

--- src/content/friends.ts
+++ src/content/friends.ts
@@ -6,4 +6,10 @@ interface FriendshipLink {
 }
 
 export const friendshipLinks: FriendshipLink[] = [
+  {
+    url: 'https://www.htmacg.cn',
+    avatarUrl: 'https://www.htmacg.cn/wp-content/uploads/2025/09/cropped-RC-LOGO_icon-192x192.png',
+    title: '墨韵先知社',
+    description: '一砚墨香融万象，半窗书影纳千言',
+  },
 ]
