e6bdacebc59d911b786f3f8a8fcee3c9dba937b1
DuckBurnIncense <duckburnincense@outlook.com>
2025-10-24 11:23:32 +0800

    修改 tailwind.config.cjs 以试图修复 cloudflare 偶发首次构建失败的 bug

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+  content: [
+    "./src/styles/main.css",
+    "./src/styles/**/*.css",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"
+  ],
   darkMode: "class", // allows toggling dark mode manually
   theme: {
     extend: {
